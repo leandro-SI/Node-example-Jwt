493ba14ad450000d34592c2a56b55ee82928ccee
Leandro-SI <llmanutencao@bol.com.br>
2022-03-11 11:07:47 -0300

    gerar token

---
+++ src/services/auth.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken')
+const config = require ('../config')
+
+const generateToken = (payload) => {
+    return jwt.sign(payload, config.privateKey, config.authOptions)
+}
+
+module.exports = {
+    generateToken,
+}
